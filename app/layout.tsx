4672c1a0d773eac6c85cbd3b9cb41ddc5549456e
Raphaël Badia <raphael@badia.cc>
2023-07-21 17:38:21 +0200

    show that using headers() in generateMetadata doesn't work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
+import { headers } from "next/headers";
+
+export async function generateMetadata() {
+  const host = headers().get("host");
+  return {
+    title: `Title ${host}`,
+    description: `This is the description for ${host}`,
+  };
+}
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
